682ea0511ae8c5827d12f599c29a951db097756d
mariadalid <20243rd014@utez.edu.mx>
2025-07-10 08:51:39 -0600

    Se modifico pestaña de admin para mostrar fechas de asignacion

--- src/main/java/com/example/proyecto1/AdminController.java
+++ src/main/java/com/example/proyecto1/AdminController.java
@@ -4,6 +4,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import org.w3c.dom.ls.LSOutput;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+
 
 public class AdminController {
 
@@ -15,8 +24,17 @@ public class AdminController {
     @FXML private Button btnHorario;
     @FXML private Button btnSalir;
     @FXML private Button btnAdmin;
+    @FXML private Label lblFecha;
 
+    public void onAsignarFecha() {
+        LocalDate fechaSeleccionada = fecha_inicio.getValue();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM");
+            String fechaInicio = fechaSeleccionada.format(formatter);
 
+        LocalDate fechaSeleccionadaFinal = fecha_cierre.getValue();
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd-MM");
+        String fechaCierre = fechaSeleccionadaFinal.format(formatter1);
+        lblFecha.setText(fechaInicio +"---"+ fechaCierre);
 
-
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.proyecto1 {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.xml;
 
     opens com.example.proyecto1 to javafx.fxml;
     exports com.example.proyecto1;
