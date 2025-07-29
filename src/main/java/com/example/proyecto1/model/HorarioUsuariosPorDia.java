d733d1852bfa75d380b6fd5a10fc5979a1f31ca5
MarioPachecoHer <20243rd005@utez.edu.mx>
2025-07-29 10:51:15 -0600

    Se agregó @SerializedName para mapear   correctamente la clave de 'horario' para poder utilizarlo con gson.

--- src/main/java/com/example/proyecto1/model/HorarioUsuariosPorDia.java
+++ src/main/java/com/example/proyecto1/model/HorarioUsuariosPorDia.java
@@ -1,44 +1,41 @@
 package com.example.proyecto1.model;
 
-
 import com.google.gson.annotations.SerializedName;
 import java.util.List;
 import java.util.Map;
 
 public class HorarioUsuariosPorDia {
 
-        private String correo;
 
-        @SerializedName("horario")
-        private Map<String, List<Integer>> horarioMap;
+    private String correo;
 
-        // Constructor
-        public HorarioUsuariosPorDia(String correo, Map<String, List<Integer>> horarioMap) {
-            this.correo = correo;
-            this.horarioMap = horarioMap;
-        }
+    @SerializedName("horario")
+    private Map<String, List<Integer>> horarioMap;
 
-        // Getter: correo
-        public String getCorreo() {
-            return correo;
-        }
 
-        // Setter: correo
-        public void setCorreo(String correo) {
-            this.correo = correo;
-        }
-
-        // Getter: mapa de horarios
-        public Map<String, List<Integer>> getHorarioMap() {
-            return horarioMap;
-        }
-
-        // Setter: mapa de horarios
-        public void setHorarioMap(Map<String, List<Integer>> horarioMap) {
-            this.horarioMap = horarioMap;
-        }
+    public HorarioUsuariosPorDia(String correo, Map<String, List<Integer>> horarioMap) {
+        this.correo = correo;
+        this.horarioMap = horarioMap;
     }
 
-    //hola
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+
+    public Map<String, List<Integer>> getHorarioMap() {
+        return horarioMap;
+    }
+
+
+    public void setHorarioMap(Map<String, List<Integer>> horarioMap) {
+        this.horarioMap = horarioMap;
+    }
+}
 
 
