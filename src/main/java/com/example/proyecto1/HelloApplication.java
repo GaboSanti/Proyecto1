a7a6ef14aa4064b904cd10f6a20304ed5989a2aa
MarioPachecoHer <20243rd005@utez.edu.mx>
2025-08-09 21:34:26 -0600

    Se ajusto la pantalla para que salga ajustado al fxml.

--- src/main/java/com/example/proyecto1/HelloApplication.java
+++ src/main/java/com/example/proyecto1/HelloApplication.java
@@ -11,23 +11,17 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 832);
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
+        stage.sizeToScene();
+        stage.setResizable(false);
 
-        stage.setMaximized(true);
 
 
-        //SEGUNDA PANTALLA, INVESTIGAR COMO PASAR A LA OTRA PANTALLA APARTIR DE UN BOTON Y QUE NO SE ABRAN AL MISMO TIEMPO
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Registro.fxml"));
-        Parent root = loader.load();
-        Stage nuevoStage = new Stage();
-        nuevoStage.setTitle("Horario");
-        nuevoStage.setScene(new Scene(root));
-        nuevoStage.show();
-
-        stage.setMaximized(true);
     }
 
     public static void main(String[] args) {
